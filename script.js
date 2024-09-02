47dc5cccb43cd75693ae1123a535b2ef2b72eeb0
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-02 07:48:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fname=document.querySelector("#fname").value;
+	let lname=document.querySelector("#lname").value;
+	alert(`${fname} ${lname}`);
 
 }
